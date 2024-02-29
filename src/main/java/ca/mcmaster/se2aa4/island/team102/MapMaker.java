d14ad7dc13e7596fe7de98125ff02803b2a03d07
Skilledgamer101 <mansoorlunawadi@yahoo.ca>
2024-02-29 09:39:44 -0500

    using linkedhashmap so same ground is followed (optimized path to ground)

--- src/main/java/ca/mcmaster/se2aa4/island/team102/MapMaker.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team102/MapMaker.java
@@ -1,14 +1,14 @@
 package ca.mcmaster.se2aa4.island.team102;
 
-import java.util.HashMap;
+import java.util.Set;
 import java.util.Objects;
-
+import java.util.LinkedHashMap;
 import org.json.JSONObject;
 import ca.mcmaster.se2aa4.island.team102.Compass.Heading;
 
 public class MapMaker {
-
-    HashMap<Heading, JSONObject> map = new HashMap<>();
+    int map_capacity = 4;
+    LinkedHashMap<Heading, JSONObject> map = new LinkedHashMap<>(map_capacity);
     Heading best_direction;
 
     public void put(Heading heading, JSONObject extraInfo) {
@@ -21,16 +21,16 @@ public class MapMaker {
 
     public void choose() {
         Integer smallest_out_of_range = 1000;
+        Set<Heading> keys = map.keySet();
 
-        for (HashMap.Entry<Heading, JSONObject> entry : map.entrySet()) {
-            Heading direction = entry.getKey();
-            JSONObject extraInfo = entry.getValue();
+        for (Heading direction : keys) {
+            JSONObject extraInfo = map.get(direction);
             // type should be "OUT_OF_RANGE" or "GROUND"
             String type = extraInfo.getString("found");
             // range should be an Integer (how far out of range or ground is)
             Integer range = extraInfo.getInt("range");
             // return range for ground as soon as we get it
-            // since we know there will be only one ground in 4 directions
+            // same direction of ground will be given in orderedmap
             if (Objects.equals(type,"GROUND")) {
                 this.best_direction = direction;
                 break;
