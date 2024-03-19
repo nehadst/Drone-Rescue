8066e46d7dfd57bb8e9ff7de2f189f913e8b843d
HashBukhtiar <hashbukhtiar@gmail.com>
2024-03-19 13:06:49 -0400

    Added comments explaining turn and fly methods #41

--- src/main/java/ca/mcmaster/se2aa4/island/team102/Drone.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team102/Drone.java
@@ -10,6 +10,10 @@ public class Drone {
     State currentState;
 
     public JSONObject turn(Heading face) {
+        // (Heading) -> JSONObject
+        // This method returns a JSONObject command to make the drone turn in the 
+        // direction of the given heading
+
         JSONObject decision = new JSONObject();
         JSONObject extra_parameters = new JSONObject();
         decision.put("action", "heading");
@@ -19,6 +23,8 @@ public class Drone {
     }
 
     public JSONObject fly() {
+        // () -> JSONObject
+        // This method returns the command to make the drone fly forward
         JSONObject decision = new JSONObject();
         decision.put("action", "fly");
         return decision;
