b240fbd8a3f5bf0974a10427b010bec3178a3576
Nehad <nehad.st@gmail.com>
2024-03-20 13:44:04 -0400

    Added comments to get_biomes, get_creeks, and get_sites methods, #41

--- src/main/java/ca/mcmaster/se2aa4/island/team102/ScanParser.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team102/ScanParser.java
@@ -8,16 +8,22 @@ public class ScanParser implements ScanParsing {
 //Parses scanning results from JSON objects to extract specific information.    
     @Override
     public JSONArray get_biomes(JSONObject extraInfo) {
+    // (JSONObject) -> JSONArray
+    // Extracts and returns the array of biomes from the given JSON object containing scan information.    
         JSONArray biomes = extraInfo.getJSONArray("biomes");
         return biomes;
     }
     @Override
     public JSONArray get_creeks(JSONObject extraInfo) {
+    // (JSONObject) -> JSONArray
+    // Extracts and returns the array of creek IDs from the given JSON object containing scan information.   
         JSONArray creeks = extraInfo.getJSONArray("creeks");
         return creeks;
     }
     @Override
     public JSONArray get_sites(JSONObject extraInfo) {
+    // (JSONObject) -> JSONArray
+    // Extracts and returns the array of site IDs from the given JSON object containing scan information.    
         JSONArray sites = extraInfo.getJSONArray("sites");
         return sites;
     }
