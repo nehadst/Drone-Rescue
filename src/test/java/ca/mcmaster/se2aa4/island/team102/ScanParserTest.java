db854e110f07479c730757e6ae5b388e08105cf6
Skilledgamer101 <mansoorlunawadi@yahoo.ca>
2024-03-20 12:16:24 -0400

    created tests for scanparser class <close #46>

---
+++ src/test/java/ca/mcmaster/se2aa4/island/team102/ScanParserTest.java
@@ -0,0 +1,44 @@
+package ca.mcmaster.se2aa4.island.team102;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ScanParserTest {
+
+    ScanParser testParser;
+    JSONObject testJSONExtras;
+
+    @BeforeEach
+    public void initJSON() {
+        testParser = new ScanParser();
+        testJSONExtras = new JSONObject()
+            .put("biomes", new JSONArray(List.of("GLACIER", "ALPINE")))
+            .put("creeks", new JSONArray(List.of("creekid")))
+            .put("sites", new JSONArray(List.of("ERid")));
+    }
+
+    @Test
+    public void shouldGetBiomes() {
+        assertEquals(testParser.get_biomes(testJSONExtras).toString(), new JSONArray(List.of("GLACIER", "ALPINE")).toString());
+    }
+
+    @Test
+    public void shouldGetCreeks() {
+        assertEquals(testParser.get_creeks(testJSONExtras).toString(), new JSONArray(List.of("creekid")).toString());
+    }
+
+    @Test
+    public void shouldGetSites() {
+        assertEquals(testParser.get_sites(testJSONExtras).toString(), new JSONArray(List.of("ERid")).toString());
+    }
+
+
+
+}
