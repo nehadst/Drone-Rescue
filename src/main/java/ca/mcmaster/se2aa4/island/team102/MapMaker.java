4a9c4f442d5158cf74b22fac3029e1b3f4b1146c
Nehad <nehad.st@gmail.com>
2024-03-20 13:25:24 -0400

    Added comments to MapMaker class, put method, MapMaker method, and is_stuck method, #41

--- src/main/java/ca/mcmaster/se2aa4/island/team102/MapMaker.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team102/MapMaker.java
@@ -10,6 +10,7 @@ import org.json.JSONObject;
 import java.util.Optional;
 
 public class MapMaker {
+//This class is used for tracking and manipulating the map of the simulated environment.    
     int map_capacity = 3;
     LinkedHashMap<Heading, JSONObject> map = new LinkedHashMap<>(map_capacity);
     Heading best_direction;
@@ -17,15 +18,21 @@ public class MapMaker {
     Compass map_compass;
 
     public void put(Heading heading, JSONObject extraInfo) {
+    //(Heading, JSONObject) -> void
+    //Puts the heading and extraInfo into the map.    
         map.put(heading, extraInfo);
     }
 
     public MapMaker (Heading initial_heading, Compass compass) {
+    //(Heading, Compass) -> MapMaker
+    //Initializes a MapMaker with initial_heading and compass.    
         this.best_direction = initial_heading;
         this.map_compass = compass;
     }
 
     public boolean is_stuck() {
+    //() -> boolean
+    //Determines if the current state is stuck based on visited locations and map information.    
         List<Heading> keys = new ArrayList<>(map.keySet());
 
         for (Heading direction : keys) {
