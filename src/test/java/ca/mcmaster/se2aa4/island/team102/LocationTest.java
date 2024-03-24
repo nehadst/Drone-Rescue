f3dae73d8850b1bf7afb51b3b5b7352389fb440e
Nehad <nehad.st@gmail.com>
2024-03-24 17:57:59 -0400

    Added LocationTest comments, #64

--- src/test/java/ca/mcmaster/se2aa4/island/team102/LocationTest.java
+++ src/test/java/ca/mcmaster/se2aa4/island/team102/LocationTest.java
@@ -4,19 +4,26 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LocationTest {
+    // Test class for Location actions.
     private Location testLocation;
 
     @BeforeEach
     public void initLocation() {testLocation = new Location(0, 0);}
+    // () -> void
+    //This method sets up the test environment before each test by initializing a Location at (0, 0).
 
     @Test
     public void shouldUpdate() {
+        //() -> void
+        //This test verifies that the update method correctly updates the location to (1, 1).
         testLocation.update(new Location(1,1));
         assertEquals(testLocation, new Location(1, 1));
     }
 
     @Test
     public void shouldCalculate() {
+        //() -> void
+        //This test verifies that the calculate method correctly calculates the location (1, 1) from the current location (0, 0).
         Location testCalc = testLocation.calculate(new Location(1, 1));
         assertEquals(testCalc, new Location(1, 1));
     }
